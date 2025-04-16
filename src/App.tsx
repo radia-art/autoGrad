cfefb470fcf8748021f110118e113b66cab4809f
Radia Bahaya <radiabahaya@gmail.com>
2025-04-16 09:56:11 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Index />} />
+          <Route path="/register" element={<Index />} />
+          <Route 
+            path="/dashboard" 
+            element={
+              <>
+                <Header />
+                <Dashboard />
+              </>
+            } 
+          />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+        <Sonner />
+      </TooltipProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
+
+export default App;
